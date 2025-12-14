12cf9eb7d445889c3661c07c8b2f83830d65fa25
hackedsdq <hackedsdq@gmail.com>
2025-12-15 00:49:34 +0100

    fix small bug

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 export async function createClient() {
   const cookieStore = await cookies()
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY as string
   return createServerClient(
     supabaseUrl,
     supabaseAnonKey,
