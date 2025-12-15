8003cd7a1d534ca46be2275d7394e252058e00d3
hackedsdq <hackedsdq@gmail.com>
2025-12-15 01:27:52 +0100

    fix bug

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,7 +9,7 @@ const Login = () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `http://localhost:3000/auth/callback`,
+        redirectTo: `https://taplance.vercel.app/auth/callback`,
       },
     });
   };
