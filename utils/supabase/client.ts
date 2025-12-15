076d7f314044fc98ad1ec294956122cb56dd6bc1
hackedsdq <hackedsdq@gmail.com>
2025-12-15 01:13:48 +0100

    fix bug

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -7,6 +7,6 @@ export const createClient = () =>
   createBrowserClient<Database>(
     // Pass Supabase URL and anonymous key from the environment to the client
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 export type TypedSupabaseClient = SupabaseClient<Database>;
